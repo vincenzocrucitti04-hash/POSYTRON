5f3f7dafe09752187009e4b8d265ca5f7beae4a6
vincenzocrucitti04-hash <vincenzo.crucitti04@gmail.com>
2026-04-01 18:12:35 +0200

    modifica per efficienza del codice

--- Progetto_01_04/trello/src/components/Column.jsx
+++ Progetto_01_04/trello/src/components/Column.jsx
@@ -1,47 +1,28 @@
 import Card from "./Card";
 
-function Column({ title, tasks, id, onAdd, onDelete, onMove }) {
+function Column({ title, tasks, status, onAdd, onDelete, onMove, allStatuses }) {
   return (
-    <div style={{
-        background: "#f4f4f4",
-        padding: "30px",
-        width: "250px",
-        borderRadius: "10px"
-    }}>
-      <h2 style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        {title} 
-        <span style={{ 
-          fontSize: "14px", 
-          background: "#ddd", 
-          padding: "2px 8px", 
-          borderRadius: "10px",
-          color: "#666" 
-        }}>
-          {tasks.length}
-        </span>
-      </h2>
+    <div className="column" 
+    style={{ 
+      background: "#f4f4f4",
+      padding: "10px",
+      width: "250px",
+      borderRadius: "10px" 
+      }}>
+
+      <h2>{title} ({tasks.length})</h2>
 
       {tasks.map(task => (
         <Card 
           key={task.id} 
-          task={task}
-          columnId={id}
-          onDelete={() => onDelete(id, task.id)}
-          onMove={onMove} 
+          task={task} 
+          onDelete={() => onDelete(task.id)} 
+          onMove={onMove}
+          allStatuses={allStatuses}
         />
       ))}
 
-      <button className="buttonAdd"
-      onClick={() => onAdd(id)} 
-      style={{
-        width: "100%", 
-        marginTop: "20px", 
-        cursor: "pointer"
-        }}>
-        Aggiungi Task ( + )
-      </button>
+      <button onClick={() => onAdd(status)}>+ Aggiungi</button>
     </div>
   );
 }
-
-export default Column;
