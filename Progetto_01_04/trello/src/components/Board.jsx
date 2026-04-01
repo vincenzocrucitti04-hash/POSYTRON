39de984f5481b8d6b7fa230611ce015152b2d93b
vincenzocrucitti04-hash <vincenzo.crucitti04@gmail.com>
2026-04-01 17:15:43 +0200

    modifica per efficienza del codice

--- Progetto_01_04/trello/src/components/Board.jsx
+++ Progetto_01_04/trello/src/components/Board.jsx
@@ -1,33 +1,21 @@
 import Column from "./Column";
 
-function Board({ board, onAdd, onDelete, onMove }) {
+function Board({ tasks, columns, onAdd, onDelete, onMove }) {
   return (
     <div style={{ display: "flex", gap: "20px", justifyContent: "center" }}>
-
-      <Column 
-      title="Todo" 
-      id="todo" 
-      tasks={board.todo} 
-      onAdd={onAdd} 
-      onDelete={onDelete}
-      onMove={onMove} />
-
-      <Column 
-      title="Progress" 
-      id="progress" 
-      tasks={board.progress} 
-      onAdd={onAdd} 
-      onDelete={onDelete}
-      onMove={onMove} />
-
-      <Column 
-      title="Done" 
-      id="done" 
-      tasks={board.done} 
-      onAdd={onAdd} 
-      onDelete={onDelete}
-      onMove={onMove} />
-      
+      {columns.map(colStatus => (
+        <Column 
+          key={colStatus}
+          title={colStatus.toUpperCase()}
+          status={colStatus}
+          // Filtriamo i task: passiamo alla colonna solo quelli che le competono
+          tasks={tasks.filter(t => t.status === colStatus)} 
+          onAdd={onAdd} 
+          onDelete={onDelete} 
+          onMove={onMove}
+          allStatuses={columns}
+        />
+      ))}
     </div>
   );
 }
