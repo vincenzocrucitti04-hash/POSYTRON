4db60bf3afe7acc057af2d18fcfe2f145aae0a9d
vincenzocrucitti04-hash <vincenzo.crucitti04@gmail.com>
2026-04-01 18:16:00 +0200

    modifica per efficienza del codice

--- Progetto_01_04/trello/src/components/Card.jsx
+++ Progetto_01_04/trello/src/components/Card.jsx
@@ -1,44 +1,32 @@
-function Card({ task, columnId, onDelete, onMove }) {
+function Card({ task, onMove, onDelete, allStatuses }) {
   return (
-    <div style={{
+    <div className="card"
+     style={{ 
       background: "white",
       padding: "10px",
       marginBottom: "10px",
-      borderRadius: "5px",
-      display: "flex",
-      flexDirection: "column",
-      gap: "8px",
-      boxShadow: "0 2px 5px rgba(0,0,0,0.1)"
-    }}>
-      <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-      <span>{task.text}</span>
-      <button 
-        onClick={onDelete} 
-        style={{ 
-            background: "red", 
-            color: "white", 
-            border: "none", 
-            cursor: "pointer", 
-            padding: "2px 6px", 
-            borderRadius: "3px" 
-          }}
-      >
-        X
-      </button>
-      </div>
-      
-      <select 
-        value={columnId} 
-        onChange={(e) => onMove(task.id, columnId, e.target.value)}
-        style={{ fontSize: "12px", padding: "2px", cursor: "pointer" }}
-      >
-        <option value="todo">Da fare</option>
-        <option value="progress">In corso</option>
-        <option value="done">Fatto</option>
-      </select>
+      borderRadius: "5px" 
+      }}>
 
+      <div style={{
+         display: "flex",
+        justifyContent: "space-between" 
+        }}>
+
+        <span>{task.text}</span>
+        
+        <button onClick={onDelete} style={{ background: "red", color: "white" }}>X</button>
+      </div>
+
+      <select 
+        value={task.status} 
+        onChange={(e) => onMove(task.id, e.target.value)}
+        style={{ marginTop: "10px", width: "100%" }}
+      >
+        {allStatuses.map(s => (
+          <option key={s} value={s}>{s}</option>
+        ))}
+      </select>
     </div>
   );
 }
-
-export default Card;
