d89dd432be58a3282e6ac0a0f7cf61efc32554bb
vincenzocrucitti04-hash <vincenzo.crucitti04@gmail.com>
2026-04-02 16:35:42 +0200

    aggiunto componente onEdit, migliorata formattazione

--- Progetto_01_04/trello/src/components/Column.jsx
+++ Progetto_01_04/trello/src/components/Column.jsx
@@ -1,32 +1,17 @@
 import Card from "./Card";
 
-function Column({ title, tasks, description, onDelete, onMove, allStatuses }) {
+function Column({ title, tasks, description, onDelete, onMove, onEdit, allStatuses }) {
   return (
-    <div className="column" 
-    style={{ 
-      background: "#f4f4f4",
-      padding: "20px",
-      width: "250px",
-      borderRadius: "10px"
-      }}>
+    <div className="column" style={{ background: "#f4f4f4", padding: "20px", width: "250px", borderRadius: "10px" }}>
 
-      <h2 style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        fontFamily:"Playwrite IE"
-        }}>
+      <h2 style={{ display: "flex", justifyContent: "space-between", alignItems: "center", fontFamily:"Playwrite IE" }}>
 
         {title}
-        
-        <span style={{ 
-          fontSize: "14px", 
-          background: "#ddd", 
-          padding: "2px 8px", 
-          borderRadius: "10px",
-          color: "#666" 
-        }}>
+
+        <span style={{ fontSize: "14px", background: "#ddd", padding: "2px 8px", borderRadius: "10px",color: "#666" }}>
+
           {tasks.length}
+
         </span>
       </h2>
 
@@ -37,6 +22,7 @@ function Column({ title, tasks, description, onDelete, onMove, allStatuses }) {
           description={description} 
           onDelete={() => onDelete(task.id)} 
           onMove={onMove}
+          onEdit={onEdit}
           allStatuses={allStatuses}
         />
       ))}
