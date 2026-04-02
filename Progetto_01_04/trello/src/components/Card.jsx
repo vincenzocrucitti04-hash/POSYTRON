949213a820b8ca08e83226f4179921e731a7df1f
vincenzocrucitti04-hash <vincenzo.crucitti04@gmail.com>
2026-04-02 16:35:15 +0200

    aggiunto componente onEdit, migliorata formattazione

--- Progetto_01_04/trello/src/components/Card.jsx
+++ Progetto_01_04/trello/src/components/Card.jsx
@@ -1,23 +1,26 @@
-function Card({ task, onMove, onDelete, allStatuses }) {
+function Card({ task, onMove, onDelete, onEdit, allStatuses }) {
   return (
-    <div className="card"
-     style={{ 
-      background: "white",
-      padding: "10px",
-      marginBottom: "10px",
-      borderRadius: "5px" 
-      }}>
-
-      <div style={{
-         display: "flex",
-         flexDirection:"column"
-        }}>
+    <div className="card" style={{ background: "white", padding: "10px", marginBottom: "10px", borderRadius: "5px" }}>
+      <div style={{ display: "flex", flexDirection:"column"}}>
 
         <span style={{fontWeight: "bold", marginBottom: "15px"}}>{task.text}</span>
 
         <span style={{marginBottom: "15px"}}>{task.description}</span>
 
-        <button onClick={onDelete} style={{ background: "red", color: "white" }}>X</button>
+        <button onClick={onDelete} className="delete-btn" style={{ background: "red", color: "white", cursor: "pointer", borderRadius: "3px", border: "none", marginBottom: "5px"}}>X</button>
+
+        <button onClick={() => onEdit(task)} className="edit-btn">✎</button>
+      </div>
+
+      <div style={{ fontSize: "10px", color: "#999", marginTop: "10px" }}>
+
+        <div>Creato il: {task.createdAt || "N/D"}</div>
+        
+        {task.updatedAt && (
+          <div style={{ color: "#007bff", fontWeight: "500", marginTop: "2px" }}>
+            Ultima modifica: {task.updatedAt}
+          </div>
+        )}
       </div>
 
       <select 
